9ee64146e3553327e89e5831e777edbe819907ff
Geir Arne Hjelle <geirarne@gmail.com>
2021-11-07 00:16:46 +0100

    Add copier template

---
+++ README.md
@@ -0,0 +1,20 @@
+# Advent of Code Python Template for Copier
+
+This template creates scaffolding for one day of [Advent of Code](https://adventofcode.com/). It includes tests and can download your personal input data if you have [`advent-of-code-data`](https://pypi.org/project/advent-of-code-data/) installed.
+
+## Quick Start
+
+The first time you use this template you should make sure that you have [Copier](https://copier.readthedocs.io/) installed and optionally `advent-of-code-data` as well. You can install these with pipx and pip:
+
+```console
+$ pipx install copier
+$ python -m pip install advent-of-code-data
+```
+
+Once you have Copier on your system, you can create Advent of Code solution templates as follows:
+
+```console
+$ copier https://github.com/gahjelle/template-aoc-python advent_of_code
+```
+
+This will ask you about which **year** and **day** you want to template. The files are copied into a subdirectory of the `advent_of_code` directory on your computer.
---
+++ src/download_input.py
@@ -0,0 +1,45 @@
+"""Download input for one Advent of Code puzzle if possible
+
+Uses https://pypi.org/project/advent-of-code-data/ if it's available.
+Otherwise, does nothing.
+"""
+
+# Standard library imports
+import pathlib
+import sys
+
+# Third party imports
+try:
+    from aocd.models import Puzzle
+except ImportError:
+    pypi_url = "https://pypi.org/project/advent-of-code-data/"
+    print(f"Install {pypi_url} to autodownload input files")
+    raise SystemExit()
+
+
+def download(year, day):
+    """Get input and write it to input.txt inside the puzzle folder"""
+    puzzle = Puzzle(year=year, day=day)
+
+    # Download input
+    year_path = pathlib.Path(__file__).parent / str(year)
+    output_path = next(year_path.glob(f"{day:02d}*")) / "input.txt"
+    output_path.write_text(puzzle.input_data)
+
+    # Add README with link to puzzle text
+    readme_path = output_path.with_name("README.md")
+    readme_path.write_text(
+        f"# {puzzle.title}\n\n"
+        f"**Advent of Code: Day {day}, {year}**\n\n"
+        f"See {puzzle.url}"
+    )
+
+
+if __name__ == "__main__":
+    try:
+        # Read year and day from command line
+        download(year=int(sys.argv[1]), day=int(sys.argv[2]))
+    except Exception as err:
+        # Catch exceptions so that Copier doesn't clean up directories
+        print(f"Download of input failed: {err}")
+        raise SystemExit()
